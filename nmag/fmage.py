4bc40c15bf5e7a5743fa5fa110e9b6db4986b065
sichao Y <13816901408@163.com>
2024-02-03 03:33:58 +0800

    feat: add logging info

--- nmag/fmage.py
+++ nmag/fmage.py
@@ -10,5 +10,9 @@ def makedir(path, mirror_rule=("0_MOOC_Videos", "Drive/sync/0_notes_all/0_MOOC_n
         print(f"based on rule {mirror_rule}, this path is mirrorable")
         mirror_path = full_path.replace(mirror_rule[0], mirror_rule[1])
         print(f"make its mirror dir: {mirror_path}")
-        assert osp.exists(osp.dirname(mirror_path))
+        if not osp.exists(osp.dirname(mirror_path)):
+            raise ValueError(
+                "the parent directory on mirror path doesnot exist, please check!",
+                " this is normaly due to parent path inconsistency btw. primal and mirror",
+            )
         os.makedirs(mirror_path)
